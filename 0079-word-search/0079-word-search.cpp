7b31d228b42166f63cba000c08011ad034502cd4
Shubham Rawat <96018275+rwt-2001@users.noreply.github.com>
2022-10-13 20:45:39 +0530

    Time: 289 ms (90.82%), Space: 7.9 MB (94.39%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    bool isAvailable(string & word, vector<vector<char>> & board, int i, int j, int index , vector<vector<int>> &isVisited){
+    bool isAvailable(string & word, vector<vector<char>> & board, int i, int j, int index ){
         if(index>=word.size()) return true;
-        if(i<0 || i>=board.size() || j < 0 || j>=board[0].size()|| isVisited[i][j] == 1 || board[i][j]!=word[index]) return false;
+        if(i<0 || i>=board.size() || j < 0 || j>=board[0].size()|| board[i][j]!=word[index]) return false;
         
-        isVisited[i][j] = 1;
+        board[i][j] = '0';
         
         bool isWordAvailable = false;
         
-        isWordAvailable = isAvailable(word, board, i-1,j,index+1, isVisited) || isAvailable(word, board, i,j-1,index+1, isVisited) || isAvailable(word, board, i,j+1,index+1, isVisited) || isAvailable(word, board, i+1,j,index+1, isVisited);
+        isWordAvailable = isAvailable(word, board, i-1,j,index+1) || isAvailable(word, board, i,j-1,index+1) || isAvailable(word, board, i,j+1,index+1) || isAvailable(word, board, i+1,j,index+1);
         
-        isVisited[i][j] = 0;
+        board[i][j] = word[index];
         
         return isWordAvailable;
         
     }
     bool exist(vector<vector<char>>& board, string word) {
-        vector<vector<int>> isVisited(board.size(), vector<int> (board[0].size(), 0));
+        
         for(int i = 0;i<board.size();i++){
             for(int j = 0;j<board[0].size();j++){
                 
-                if(isAvailable(word, board, i,j,0, isVisited)) return 1;
+                if(isAvailable(word, board, i,j,0)) return 1;
                 
             }
         }
