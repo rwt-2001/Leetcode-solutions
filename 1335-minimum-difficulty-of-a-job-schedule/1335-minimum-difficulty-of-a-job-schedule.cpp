b4d640e0de00f567b6026208a6d0da6871d5b756
Shubham Rawat <96018275+rwt-2001@users.noreply.github.com>
2022-10-16 15:23:08 +0530

    Time: 128 ms (35.35%), Space: 187.3 MB (5.09%) - LeetHub

---
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int solve(vector<int> &j, int index, int curMax, int d, vector<vector<vector<int>>> &dp){
+        // cout<<index<<" "<<index<<endl;
+        
+        if(index==j.size()) return 300000;
+        
+        if(d==1){
+            return *max_element(j.begin() + index, j.end());
+        }
+        
+        
+        curMax = max(curMax, j[index]);
+        if(dp[index][d][curMax] != -1) return dp[index][d][curMax];
+        
+        int pick = curMax + solve(j, index+1, INT_MIN, d-1, dp);
+        
+        int notPick = solve(j, index+1, curMax, d, dp);
+        
+        return dp[index][d][curMax] = min(pick, notPick);
+        
+        
+    }
+    int minDifficulty(vector<int>& j, int d) {
+        if(d > j.size()) return -1;
+        vector<vector<vector<int>>> dp(j.size(), vector<vector<int>> (d+1, vector<int> ( 1002, -1)));
+        return solve(j, 0, INT_MIN, d, dp);
+    }
+    
+};
