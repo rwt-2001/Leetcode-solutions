0260823d7fbd418f5156a04a2bc2ef5685477a7e
Shubham Rawat <96018275+rwt-2001@users.noreply.github.com>
2022-10-22 15:12:30 +0530

    Time: 19 ms (80.58%), Space: 7.7 MB (68.10%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,44 +1,37 @@
 class Solution {
 public:
-    
-    bool areEqual(vector<int> &sm, vector<int> &tm){
-        
-        for(int i = 0;i<256;i++){
-            if(tm[i] && tm[i] > sm[i]) return false;
-        }    
-        return true;
-    }
-    
     string minWindow(string s, string t) {
-        vector<int> sm(256), tm(256);
+        vector<int> mp(256);
         for(auto &c : t){
-            tm[c]++;
+            mp[c]++;
         }
+        int count = t.size();
+        int i = 0;
         
-        int i = 0,j = 0;
         int start = 0, end = INT_MAX;
-        
-        while(j<s.size()){
+        for(int j = 0;j<s.size();j++){
+            if(mp[s[j]] > 0) count--;
+            mp[s[j]]--;
             
-            sm[s[j]]++;
-            
-            while(areEqual(sm, tm) && i<=j){
-                
-                if((end - start) > (j - i)){
+            while(count == 0){
+                if(end - start > j - i){
                     start = i;
                     end = j;
                 }
                 
-                sm[s[i]]--;
+                if(mp[s[i]] == 0){
+                    count++;
+                }
+                mp[s[i]]++;
                 i++;
             }
-            j++;
             
         }
-
+        // cout<<start<<" "<<end;
+        
+        return end==INT_MAX ? "" : s.substr(start, end - start + 1);
         
         
-        return end != INT_MAX ? s.substr(start, end - start+1) : "";
         
     }
 };
