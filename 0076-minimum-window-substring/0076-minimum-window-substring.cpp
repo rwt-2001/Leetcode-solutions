aac52030e61f17c9201f85ebfab2835f25ffe993
Shubham Rawat <96018275+rwt-2001@users.noreply.github.com>
2022-10-22 14:38:58 +0530

    Time: 384 ms (6.42%), Space: 8.3 MB (29.52%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,42 +1,44 @@
 class Solution {
 public:
-    bool isEqual(vector<int> &t, vector<int> &f){
+    
+    bool areEqual(vector<int> &sm, vector<int> &tm){
+        
         for(int i = 0;i<256;i++){
-            if(f[i] < t[i]) return false;
-        }
+            if(tm[i] && tm[i] > sm[i]) return false;
+        }    
         return true;
     }
-    string minWindow(string s, string target) {
-        vector<int> t(256);
-        vector<int> f(265);
-        
-        for(auto &c : target){
-            t[c]++;
+    
+    string minWindow(string s, string t) {
+        vector<int> sm(256), tm(256);
+        for(auto &c : t){
+            tm[c]++;
         }
         
+        int i = 0,j = 0;
+        int start = 0, end = INT_MAX;
         
-        int i = 0;
-        int start= 0, end=INT_MAX;
-        
-        for(int j = 0;j<s.size();j++){
+        while(j<s.size()){
             
-            if(t[s[j]]) f[s[j]]++;
+            sm[s[j]]++;
             
-            while(isEqual(t, f)){
+            while(areEqual(sm, tm) && i<=j){
                 
-                if(end - start > j - i){
+                if((end - start) > (j - i)){
                     start = i;
                     end = j;
                 }
                 
-                if(t[s[i]]) f[s[i]]--;
+                sm[s[i]]--;
                 i++;
             }
-            
+            j++;
             
         }
+
         
-        return end==INT_MAX ? "" : s.substr(start, end-start+1);
+        
+        return end != INT_MAX ? s.substr(start, end - start+1) : "";
         
     }
 };
