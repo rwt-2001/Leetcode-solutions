44dd03c218588dfe3fd33e0933896fef8d2d055b
Shubham Rawat <96018275+rwt-2001@users.noreply.github.com>
2022-11-05 20:55:20 +0530

    Time: 1558 ms (38.89%), Space: 16.1 MB (16.99%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.cpp
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -1,88 +1,78 @@
 class Node{
-    Node *links[26];
-    bool end = false;
-    
+    Node * links[26];
+    bool isEnd = false;
     
     public:
-
-    bool hasLink(int index){
-        return links[index] != NULL;
+    
+    bool contains(char c){
+        return links[c-'a'] != NULL;
     }
     
-    void createLink(int index){
-        links[index] = new Node();
+    void createLink(char c){
+        links[c-'a'] = new Node();
     }
     
-    Node* getLink(int index){
-        return links[index];
+    Node * getLink(char c){
+        return links[c-'a'];
+    }
+    
+    bool getEnd(){
+        return isEnd;
     }
     
     void setEnd(){
-        end  = true;
-    }
-    
-    bool isEnd(){
-        return end;
+        isEnd = true;
     }
     
 };
-
 class Solution {
+    unordered_map<string, int> mp;
+    Node * root = new Node();
     vector<string> ans;
-    unordered_map<string,int> mp; 
-    Node* root = new Node();
-public:
-    void insertWord(string &word){
-        Node *temp = root;
-        
-        for(int i = 0;i<word.size();i++){
-            int index = word[i] - 'a';
-            if(!temp->hasLink(index)){
-                temp->createLink(index);
+    void insert(string &word){
+        Node * temp = root;
+        for(auto &c : word){
+            
+            if(!temp->contains(c)){
+                temp->createLink(c);
             }
-            temp = temp->getLink(index);
+            
+            temp = temp->getLink(c);
         }
-        
-        
-        
         temp->setEnd();
     }
     
-    
-    void dfs(vector<vector<char>>& board, int i, int j , Node * node, string curWord){
-        if(i < 0 || j < 0 || i==board.size() || j==board[0].size() || board[i][j] == '0' || !node->hasLink(board[i][j] - 'a')) return;
-        curWord += board[i][j];
-        // cout<<curWord<<endl;
-        int index = board[i][j] - 'a';
-        if(node->getLink(index)->isEnd() && !mp[curWord]) {
-            mp[curWord] = 1;
-            ans.push_back(curWord);
-            
-        }
+    void dfs(vector<vector<char>>& board, int i, int j, string curWord, Node *curNode){
         
+        
+        if(i < 0 || j < 0 || i == board.size() || j == board[0].size() || board[i][j] =='0'||!curNode->contains(board[i][j])) return;
+        
+        curNode = curNode->getLink(board[i][j]);
+        curWord += board[i][j];
+        if(curNode->getEnd() && mp[curWord]){
+            mp[curWord]--;
+            ans.push_back(curWord);
+        }
         board[i][j] = '0';
         
-        dfs(board, i  + 1, j, node->getLink(index), curWord);
-        dfs(board, i, j+1, node->getLink(index), curWord);
-        dfs(board, i-1, j, node->getLink(index), curWord);
-        dfs(board, i, j-1, node->getLink(index), curWord);
-        board[i][j] =curWord[curWord.size() - 1] ;
+        dfs(board, i+1, j, curWord, curNode);
+        dfs(board, i-1, j, curWord, curNode);
+        dfs(board, i, j+1, curWord, curNode);
+        dfs(board, i, j-1, curWord, curNode);
         
+        board[i][j] = curWord[curWord.size() - 1];
     }
-    
-    
+public:
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-        
-        
-        for(int i = 0;i<words.size();i++) insertWord(words[i]);
-        
-        for(int i = 0;i<board.size();i++){
-            for(int j = 0;j<board[0].size();j++){
-                dfs(board, i,j,root,"");
-                if(mp.size()==words.size()) return ans;
-            }
-                
+        for(auto &word : words){
+            mp[word]++;
+            insert(word);
         }
+        for(int i = 0;i<board.size();i++)
+            for(int j = 0;j<board[0].size();j++){
+                dfs(board,i,j,"",root);
+                if(ans.size() == words.size()) return ans;
+            }
         return ans;
     }
 };
