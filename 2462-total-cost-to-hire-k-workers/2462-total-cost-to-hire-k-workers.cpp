b268628e738b531292f2b926b756e6ebefd8cbdc
Shubham Rawat <96018275+rwt-2001@users.noreply.github.com>
2022-11-06 14:31:22 +0530

    Time: 338 ms (25.00%), Space: 72.8 MB (25.00%) - LeetHub

---
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    long long totalCost(vector<int>& costs, int k, int candidates) {
+        priority_queue<int, vector<int>, greater<int>> pql;
+        priority_queue<int, vector<int>, greater<int>> pqr;
+        int n = costs.size();
+        int i = 0;
+        int j = n-1;
+        
+        long long ans = 0;
+        while(i <= j && k){
+            while(i <= j && pql.size() < candidates){
+                pql.push(costs[i]);
+                i++; 
+            }
+            while(i <= j && pqr.size() < candidates){
+                pqr.push(costs[j]);
+                j--;
+            }
+            
+            if(pqr.empty() || pql.top() <= pqr.top() ){
+                ans += pql.top();
+                pql.pop();
+            }else{
+                ans += pqr.top();
+                pqr.pop();
+            }
+            k--;
+        }
+        while(k){
+            if( pqr.empty() || ( !pql.empty() && pql.top() <= pqr.top())){
+                ans += pql.top();
+                pql.pop();
+            }else {
+                ans += pqr.top();
+                pqr.pop();
+            }
+            k--;
+        }
+        return ans;
+    }
+};
