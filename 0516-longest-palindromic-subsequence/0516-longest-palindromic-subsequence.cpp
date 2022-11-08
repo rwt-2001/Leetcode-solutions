98a3432f41a5ce9e4f3131d463554c7ae94c14d6
Shubham Rawat <96018275+rwt-2001@users.noreply.github.com>
2022-11-08 16:07:57 +0530

    Time: 650 ms (5.03%), Space: 73.1 MB (10.27%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -5,12 +5,12 @@ public:
         
         vector<vector<int>> dp(n+1, vector<int>(n+1,0));
 
-        string r = s;
-        reverse(r.begin(), r.end());
+        
         int maxLen = 1;
         for(int i = 1;i<=n;i++)
             for(int j = 1;j<=n;j++){
-                if(s[i-1] == r[j-1])
+                
+                if(s[i-1] == s[s.size() - j])
                     dp[i][j] = dp[i-1][j-1] + 1;
                 else{
                       dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
