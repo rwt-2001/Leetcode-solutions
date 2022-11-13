50078a9a3556ec5c5b19f84050595087d55f4c4b
Shubham Rawat <96018275+rwt-2001@users.noreply.github.com>
2022-11-13 22:32:30 +0530

    Time: 3 ms (96.43%), Space: 7 MB (91.09%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -4,11 +4,9 @@ public:
         int n = s.size();
         int i = n-1;
         int j = n-1;
-        string ans = "";
         int flag = 0;
         while(j>=0){
             if(s[j]!=' '){
-                if(flag) ans+=' ';
                 i = j;
                 while(i>=0 && s[i]!=' '){
                     i--;
@@ -16,18 +14,44 @@ public:
                 if(i<0 || (i>=0 && s[i]==' ')) i++;
                 int temp = i;
                 
-                while(i<=j){
-                    ans+=s[i];
+                while(i<j){
+                    swap(s[i], s[j]);
+                    j--;
                     i++;
                 }
-                flag = 1;
                 j = temp-1;
             }
             
             j--;
         }
+        i = 0;
+        j = n-1;
+        while(i<j){
+            swap(s[i++], s[j--]);
+        }
+        i = 0;
+        j = 0;
+        while(j<n){
+            if(s[j]!=' '){
+                while(s[j]!=' ' && j<n){
+                    s[i++] = s[j++];
+                }
+                if(i<=n-1);
+                    s[i++] = ' ';
+            }
+            else
+                j++;
+        }
+        while(i<n){
+            s[i++] = ' ';
+        }
+        j = n-1;
+        while(j>=0 && s[j] ==' '){
+            s.pop_back();
+            j--;
+        }
         // ans.erase(ans.size()-1, 1);
-        return ans;
+        return s;
         
     }
 };
