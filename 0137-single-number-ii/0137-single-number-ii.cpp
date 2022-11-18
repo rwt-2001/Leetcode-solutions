49baab17313337c04915b6c24daaac3fb08ff1e6
Shubham Rawat <96018275+rwt-2001@users.noreply.github.com>
2022-11-18 14:11:40 +0530

    Time: 10 ms (87.55%), Space: 9.6 MB (37.45%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        vector<int> mp(32);
-        int n = 0;
-        for(long long num: nums){
-            int index = 0;
-            if(num < 0){
-                num = abs(num);
-                n++;
-            }
-            while(num){
-                if(num&1){
-                    mp[index]++;
+        unsigned int ans = 0;
+        
+        for(int i = 0;i<32;i++){
+            int cnt = 0;
+            for(unsigned int num : nums){
+                if(num & (1<<i)){
+                    cnt++;
                 }
-                index++;
-                num>>=1;
+                
+            }
+            
+            if(cnt%3){
+                ans |= (1<<i);
             }
         }
+        return ans;
         
-        long long ans = 0;
-        int index = 0;
-        
-        for(int i = 0;i<=31;i++){
-            int one = mp[i]%3;
-            
-            ans  = (ans | (one<<i));
-        }
-        return n%3 ? -ans : ans;       
     }
 };
