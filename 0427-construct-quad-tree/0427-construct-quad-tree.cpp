2b0ae49b1e26582116ccafa4396a68ecc6887c30
Shubham Rawat <96018275+rwt-2001@users.noreply.github.com>
2023-02-27 22:10:30 +0530

    Time: 38 ms (6.16%), Space: 17.5 MB (43.84%) - LeetHub

---
+++ 0427-construct-quad-tree/0427-construct-quad-tree.cpp
@@ -0,0 +1,122 @@
+/*
+// Definition for a QuadTree node.
+class Node {
+public:
+    bool val;
+    bool isLeaf;
+    Node* topLeft;
+    Node* topRight;
+    Node* bottomLeft;
+    Node* bottomRight;
+    
+    Node() {
+        val = false;
+        isLeaf = false;
+        topLeft = NULL;
+        topRight = NULL;
+        bottomLeft = NULL;
+        bottomRight = NULL;
+    }
+    
+    Node(bool _val, bool _isLeaf) {
+        val = _val;
+        isLeaf = _isLeaf;
+        topLeft = NULL;
+        topRight = NULL;
+        bottomLeft = NULL;
+        bottomRight = NULL;
+    }
+    
+    Node(bool _val, bool _isLeaf, Node* _topLeft, Node* _topRight, Node* _bottomLeft, Node* _bottomRight) {
+        val = _val;
+        isLeaf = _isLeaf;
+        topLeft = _topLeft;
+        topRight = _topRight;
+        bottomLeft = _bottomLeft;
+        bottomRight = _bottomRight;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* solve(vector<vector<int>> &grid, int r1, int r2, int c1, int c2)
+    {
+        if(r1==r2 && c1==c2)
+        {
+            Node *leaf = new Node(grid[r1][c1], true);
+        }
+        
+        int isSame = true;
+        int value = grid[r1][c1];
+        
+        
+        for(int i = r1; i <= r2 ; i++)
+        {
+            for(int j = c1; j <=c2 ; j++)
+            {
+                if(grid[i][j] != value)
+                {
+                    isSame = false;
+                }
+            }
+        }
+        
+        if(isSame)
+        {
+            return new Node(value, true);
+        }
+        
+        Node * node = new Node(1,false);
+        
+        Node *topLeft = NULL;
+        Node *topRight = NULL;
+        Node *bottomLeft = NULL;
+        Node *bottomRight = NULL;
+        
+        
+        int rows = r2 - r1 + 1;
+        int cols = c2 - c1 + 1;
+        
+        int rowMid = r1 + rows / 2 - 1;
+        int colMid = c1 + cols / 2 - 1;
+        
+        
+        cout<<r1<<" "<<rowMid<<" "<<rowMid+1<<r2<<endl;
+        cout<<c1<<" "<<colMid<<" "<<colMid+1<<r2;
+     
+        
+        //topLeft
+      
+        topLeft = solve(grid, r1, rowMid, c1, colMid);
+        
+        
+        //topRight
+
+        topRight = solve(grid, r1, rowMid, colMid + 1, c2);
+        
+        
+        //bottomLeft
+
+        bottomLeft = solve(grid, rowMid + 1, r2, c1, colMid);
+        
+        
+        //bottomRight
+
+        bottomRight = solve(grid,rowMid + 1, r2, colMid + 1, c2);
+        
+        
+        
+        
+        node->topLeft = topLeft;
+        node->topRight = topRight;
+        node->bottomLeft = bottomLeft;
+        node->bottomRight = bottomRight;
+        
+        return node;
+        
+    }
+    Node* construct(vector<vector<int>>& grid) {
+        return solve(grid, 0, grid.size() - 1, 0, grid[0].size() - 1);
+    }
+};
