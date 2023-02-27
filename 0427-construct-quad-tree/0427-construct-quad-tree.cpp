ad18530c807b258370d3bbd22be11f062aaf9504
Shubham Rawat <96018275+rwt-2001@users.noreply.github.com>
2023-02-27 22:21:44 +0530

    Time: 19 ms (53.62%), Space: 17.5 MB (43.48%) - LeetHub

--- 0427-construct-quad-tree/0427-construct-quad-tree.cpp
+++ 0427-construct-quad-tree/0427-construct-quad-tree.cpp
@@ -49,8 +49,7 @@ public:
         
         int isSame = true;
         int value = grid[r1][c1];
-        
-        
+
         for(int i = r1; i <= r2 ; i++)
         {
             for(int j = c1; j <=c2 ; j++)
@@ -62,58 +61,31 @@ public:
             }
         }
         
-        if(isSame)
-        {
-            return new Node(value, true);
-        }
+        if(isSame) return new Node(value, true);
+        
         
         Node * node = new Node(1,false);
         
-        Node *topLeft = NULL;
-        Node *topRight = NULL;
-        Node *bottomLeft = NULL;
-        Node *bottomRight = NULL;
-        
-        
         int rows = r2 - r1 + 1;
         int cols = c2 - c1 + 1;
-        
         int rowMid = r1 + rows / 2 - 1;
         int colMid = c1 + cols / 2 - 1;
         
-        
-       
-        
         //topLeft
-      
-        topLeft = solve(grid, r1, rowMid, c1, colMid);
-        
+        node->topLeft = solve(grid, r1, rowMid, c1, colMid);
         
         //topRight
-
-        topRight = solve(grid, r1, rowMid, colMid + 1, c2);
-        
+        node->topRight = solve(grid, r1, rowMid, colMid + 1, c2);
         
         //bottomLeft
-
-        bottomLeft = solve(grid, rowMid + 1, r2, c1, colMid);
-        
+        node->bottomLeft = solve(grid, rowMid + 1, r2, c1, colMid);
         
         //bottomRight
-
-        bottomRight = solve(grid,rowMid + 1, r2, colMid + 1, c2);
-        
-        
-        
-        
-        node->topLeft = topLeft;
-        node->topRight = topRight;
-        node->bottomLeft = bottomLeft;
-        node->bottomRight = bottomRight;
+        node->bottomRight = solve(grid,rowMid + 1, r2, colMid + 1, c2);
         
         return node;
-        
     }
+    
     Node* construct(vector<vector<int>>& grid) {
         return solve(grid, 0, grid.size() - 1, 0, grid[0].size() - 1);
     }
