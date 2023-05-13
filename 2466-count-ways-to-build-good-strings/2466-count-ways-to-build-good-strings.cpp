f7b05fdbd8550c5c7f161c150c079618d7c37fcc
Shubham Rawat <96018275+rwt-2001@users.noreply.github.com>
2023-05-13 23:06:15 +0530

    Time: 15 ms (74.21%), Space: 18.7 MB (25.79%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -1,22 +1,21 @@
 class Solution {
     int mod = 1e9 + 7;
 public:
-    int solve(int low, int high, int zero, int one, int len, int choosen, vector<vector<int>> &dp)
+    int solve(int low, int high, int zero, int one, int len, vector<int> &dp)
     {
         if(len > high) return 0;
         
-        if(dp[len][choosen] != -1) return dp[len][choosen];
+        if(dp[len] != -1) return dp[len];
         long long ans = 0;
+        ans += solve(low, high, zero, one, len + zero, dp) %mod;
+        ans += solve(low, high, zero, one, len + one, dp)%mod;
         
-        ans += solve(low, high, zero, one, len + zero, 0, dp) %mod;
-        ans += solve(low, high, zero, one, len + one, 1, dp)%mod;
-        
-        return dp[len][choosen] = (ans + (len >= low && len <= high))%mod;
+        return dp[len] = (ans + (len >= low && len <= high))%mod;
         
     }
     int countGoodStrings(int low, int high, int zero, int one) {
-        vector<vector<int>> dp(high+1, vector<int>(2, -1));
+        vector<int> dp(high+1,-1);
         
-        return (solve(low, high, zero, one, 0+one,1, dp)%mod + solve(low, high, zero, one, 0+zero,0, dp)%mod)%mod;
+        return solve(low, high, zero, one, 0, dp)%mod ;
     }
 };
