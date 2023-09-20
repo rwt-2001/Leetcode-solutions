2b4358573194954e08fe2d701dddd943405ab5ca
Shubham Rawat <96018275+rwt-2001@users.noreply.github.com>
2023-09-21 00:09:50 +0530

    Time: 116 ms (83.05%), Space: 98.8 MB (38.49%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -4,14 +4,11 @@ public:
         int target = accumulate(nums.begin(), nums.end(), 0) - x;
         if(target < 0) return -1;
         if(target == 0) return nums.size();
-        // cout<<accumulate(nums.begin(), nums.end(), 0)<<" "<<x<<" "<<target<<endl;
         int ans = -1;
         int j = 0;
         for(int i = 0; i < nums.size() ; i++)
         {
             target -= nums[i];
-            cout<<target;
-            
             while(target < 0 && j < i)
             {
                 target += nums[j++];
