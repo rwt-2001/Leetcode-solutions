b4b44ed9845dd8b1a841312a8833cefb460cb491
Shubham Rawat <96018275+rwt-2001@users.noreply.github.com>
2023-09-26 23:22:40 +0530

    Time: 3 ms (51.43%), Space: 7 MB (29.98%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        vector<int> freq(26);
-        vector<int> visited(26);
-        string ans = "";
-        int i = 0;
-        for(auto &c : s) freq[c-'a']++;
+        map<char,int> mp;
+        map<char,int> visited;
         
-        for(auto &c : s){
-           // cout<<"HERE"<<"---"<<c<<"---"<<ans<<endl;
-            while(ans.size() != 0 && ans.back() >= c && freq[ans.back()-'a']>0 &&  visited[c-'a'] == 0){
-                
-                visited[ans.back()-'a'] = 0;
-                
+        
+        for(auto c: s)mp[c]++;
+        
+        string ans="";
+        for(int i = 0; i < s.size();i++)
+        {
+            while(ans.size() && (s[i] < ans[ans.size() - 1]) && !visited[s[i]] && (mp[ans[ans.size() - 1]]>0) )
+            {
+                visited[ans[ans.size() - 1]] = 0;
                 ans.pop_back();
-                // cout<<i++<<endl;
             }
-            if(visited[c-'a'] == 0){
-                 ans.push_back(c);
-                 visited[c-'a'] = 1;
-                 
-             }
-                freq[c-'a']--;
+            
+            if(!visited[s[i]])
+            {
+                ans+=s[i];
+                visited[s[i]] = 1;
+            }
+            
+            mp[s[i]]--; 
             
         }
         
         return ans;
-        
     }
 };
+
+
