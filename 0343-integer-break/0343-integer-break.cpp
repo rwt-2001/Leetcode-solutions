af8fbff31f2e4e92daa8641a4abbe63712b2412f
Shubham Rawat <96018275+rwt-2001@users.noreply.github.com>
2023-10-06 19:07:25 +0530

    Time: 2 ms (49.96%), Space: 6.4 MB (36.38%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,22 +1,15 @@
 class Solution {
-    vector<int> dp;
+  
 public:
-    int f(int n)
-    {
-        if(n == 0) return 1;
-        if(n < 0) return -1;
-        if(dp[n]!=-1)return dp[n]; 
-        int ans = -1;
-        for(int i = 1; i <= n ; i ++)
-        {
-            ans = max(ans, f(n - i) * i);
-        }
-        return dp[n] = ans;
-    }
+
     int integerBreak(int n) {
-        dp = vector<int> (n+1, -1);
-        if(n==3) return 2;
-        if(n == 2) return 1;
-        return f(n);
+       if(n < 4) return n-1;
+        int ans = 1;
+        while(n > 4)
+        {
+            ans *= 3;
+            n-=3;
+        }
+        return ans*n;
     }
 };
