e8d4a1299d93b3b8c09afd07c8b9bd7b806f3c9c
Shubham Rawat <96018275+rwt-2001@users.noreply.github.com>
2023-10-30 17:10:20 +0530

    Time: 40 ms (5.02%), Space: 12.1 MB (5.36%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -14,11 +14,26 @@ public:
     vector<int> sortByBits(vector<int>& arr) {
         
         
-        
+        map<int,int> cache;
         sort(arr.begin(),arr.end(),[&](int a, int b)
              {
-                 int bitA = countBit(a);
-                 int bitB = countBit(b);
+                 int bitA;
+                 if(cache[a])
+                 {
+                    bitA = cache[a]; 
+                 }else{
+                     bitA = countBit(a);
+                     cache[a] = bitA;
+                 }
+                 int bitB;
+                 
+                 if(cache[b])
+                 {
+                    bitB = cache[b]; 
+                 }else{
+                     bitB = countBit(b);
+                     cache[b] = bitB;
+                 }
                  
                  if(bitA == bitB) return a < b;
                  return bitA < bitB;
