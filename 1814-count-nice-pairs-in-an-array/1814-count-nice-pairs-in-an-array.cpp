e453aa2bb12c0d8001a0c21384adab1847fee687
Shubham Rawat <96018275+rwt-2001@users.noreply.github.com>
2023-11-22 13:01:18 +0530

    Time: 175 ms (6.38%), Space: 57.1 MB (83.40%) - LeetHub

--- 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.cpp
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.cpp
@@ -13,23 +13,22 @@ public:
         return new_num;
     }
     int countNicePairs(vector<int>& nums) {
-        vector<int> rev(nums.size());
         
-        for(int i = 0;i < nums.size(); i++)
-        {
-            
-            rev[i] = rev_number(nums[i]) - nums[i];
-        }
+        
         
         unordered_map<int,int> mp;
         int ans = 0;
-        for(auto num: rev)
+       
+        for(int i = 0;i < nums.size(); i++)
         {
+            
+            int num = rev_number(nums[i]) - nums[i];
             mp[num]++;
             if(mp[num] > 1)
             {
                 ans = (ans*1L +  mp[num]-1) % mod;
             }
+        
         }
         return ans;
     }
