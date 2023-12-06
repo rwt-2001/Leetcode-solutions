f9af747e4f4c450da563d999d4673a38527d8ee2
Shubham Rawat <96018275+rwt-2001@users.noreply.github.com>
2023-12-06 21:42:18 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (70.53%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int totalMoney(int n) {
-        int i = 1;
+                int i = 1;
         int totalWeek = n / 7;
         int leftDays = n % 7;
         int ans = 0;
-        while(totalWeek)
+        if(totalWeek)
         {
-            int ft = i;
-            int lt = i+6;
-            ans += (ft+lt)*7/2;
-            i++;
-            totalWeek--;
+            int ft = 28;
+            int n = totalWeek;
+            int lt = ft + (n - 1)*7;
+            ans+= (lt+ft)*n/2;
         }
         
         if(leftDays)
         {
-            int ft = i;
-            int lt = i+leftDays - 1;
+            int ft = totalWeek + 1;
+            int lt = ft+leftDays - 1;
             ans += (ft+lt)*leftDays/2;
         }
         
