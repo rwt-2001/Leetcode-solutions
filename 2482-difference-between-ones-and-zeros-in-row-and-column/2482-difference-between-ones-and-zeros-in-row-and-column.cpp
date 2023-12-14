07cf666e3a55d5faa412711beecd4c50dd1263bf
Shubham Rawat <96018275+rwt-2001@users.noreply.github.com>
2023-12-14 21:18:59 +0530

    Time: 188 ms (76.75%), Space: 119.3 MB (42.69%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -1,34 +1,45 @@
 class Solution {
 public:
     vector<vector<int>> onesMinusZeros(vector<vector<int>>& grid) {
-        int row = grid.size();
-        int col = grid[0].size();
-        
-        vector<int> oneRow(row);
-        vector<int> zeroRow(row);
-        vector<int> oneCol(col);
-        vector<int> zeroCol(col);
+        vector<int> verOne(grid.size(), 0), verZero(grid.size(), 0), horOne(grid[0].size(), 0), horZero(grid[0].size(), 0);
         
         
-        for(int i = 0;i<row;i++){
-            for(int j = 0;j<col;j++){
-                if(grid[i][j]==0) zeroRow[i]++;
-                if(grid[i][j]==1) oneRow[i]++;
+        for(int i = 0; i < grid.size(); i++)
+        {
+            int sum0 = 0, sum1 = 0;
+            for(int j = 0;j < grid[0].size(); j++)
+            {
+                if(grid[i][j]) sum1++;
+                else sum0++;
+            }
+            verOne[i] = sum1;
+            verZero[i] = sum0;
+        }
+        
+        for(int i = 0; i < grid[0].size(); i++)
+        {
+            int sum0 = 0, sum1 = 0;
+            for(int j = 0;j < grid.size(); j++)
+            {
+                if(grid[j][i]) sum1++;
+                else sum0++;
+            }
+            horOne[i] = sum1;
+            horZero[i] = sum0;
+        }
+        
+        
+        vector<vector<int>> ans(grid.size(), vector<int>(grid[0].size(), 0));
+        
+        for(int i = 0; i < grid.size(); i++)
+        {
+            int sum0 = 0, sum1 = 0;
+            for(int j = 0;j < grid[0].size(); j++)
+            {
+               ans[i][j] = verOne[i] + horOne[j] - verZero[i] - horZero[j];
             }
         }
         
-        for(int i = 0;i<col;i++){
-            for(int j = 0;j<row;j++){
-                if(grid[j][i] == 0) zeroCol[i]++;
-                 if(grid[j][i]==1) oneCol[i]++;
-            }
-        }
-        
-        for(int i = 0;i<row;i++){
-            for(int j = 0;j<col;j++){
-                grid[i][j] = oneRow[i] + oneCol[j] - zeroRow[i] - zeroCol[j];
-            }
-        }
-        return grid;
+        return ans;
     }
 };
