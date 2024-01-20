af11196fd06a8bbaef3352eb909d8cbb984ae8a9
Shubham Rawat <96018275+rwt-2001@users.noreply.github.com>
2024-01-20 10:38:24 +0530

    Time: 67 ms (83.83%), Space: 46.4 MB (10.25%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,43 +1,47 @@
 class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
+        vector<int> minR(arr.size(), arr.size());
+        vector<int> minL(arr.size(), -1);
+        
+        
+        stack<pair<int,int>> stk;
+        for(int i = 0; i < arr.size(); i++)
+        {
+            while(stk.size() && stk.top().first > arr[i])
+            {
+                pair<int,int> topEl = stk.top();
+                minR[topEl.second] = i;
+                stk.pop();
+            }
+            stk.push({arr[i], i});
+        }
+        
+        while(stk.size()) stk.pop();
+        
+        for(int i = arr.size() - 1; i >= 0; i--)
+        {
+            while(stk.size() && stk.top().first >= arr[i])
+            {
+                pair<int,int> topEl = stk.top();
+                minL[topEl.second] = i;
+                stk.pop();
+            }
+            stk.push({arr[i], i});
+        }
+        
+        
         int mod = 1e9 + 7;
-        int n = arr.size();
-        stack<int> stk;
-        vector<int> prev(n,-1);
-        vector<int> next(n,n);
-        
-        for(int i = 0;i<n;i++){
+        long long ans = 0;
+        for(int i = 0; i < arr.size(); i++)
+        {
+            int l = minL[i];
+            int r = minR[i];
             
-            while(!stk.empty() && arr[stk.top()] >= arr[i] ){
-                next[stk.top()] = i;
-                stk.pop();
-                
-            }
-            stk.push(i);
-            
-        }
-        
-        for(int i = n-1;i>=0;i--){
-            
-            while(!stk.empty() && arr[stk.top()] > arr[i] ){
-                prev[stk.top()] = i;
-                stk.pop();
-            }
-            stk.push(i);
-        }
-        
-        
-        
-        int ans = 0;
-        
-        for(int i = 0;i<n;i++){
-              int subarrs =  ( (long long) (next[i] - i) * (long long)(i - prev[i]) ) % mod;
-                
-              ans =  ((long long)((ans % mod + (((long long)arr[i] * (long long)subarrs)) % mod)))%mod;
+            // cout<< (1l*(i-l)*(r-i))<<endl;
+            ans = (ans +  (1l*(i-l)*(r-i))*arr[i]) % mod;
         }
         
         return ans;
-        
     }
 };
