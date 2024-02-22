0775f74d8ffd00432c0a19e553d431898941a868
Shubham Rawat <96018275+rwt-2001@users.noreply.github.com>
2024-02-22 21:50:10 +0530

    Time: 110 ms (95.52%), Space: 64.4 MB (59.10%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        map<int,int> mp;
-        for(auto &t: trust)
+        vector<pair<int,int>>arr(n+1,{0,0});
+        
+        for(auto &val: trust)
         {
-            mp[t[1]]++;
-            mp[t[0]]--;
+            arr[val[0]].second++;
+            arr[val[1]].first++;
         }
         
-        
-        for(int i = 1; i <= n;i++)
-        {
-            if(mp[i]==n-1){
-                return i;
-            }
+        for(int i = 1; i < arr.size(); i++){
+            pair<int,int> val = arr[i];
+            
+            if(val.first == n-1 && val.second == 0) return i;
         }
-        
         return -1;
     }
 };
